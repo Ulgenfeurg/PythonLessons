2e9779c8fbcd03b737d85f12e0d4675dba363c9c
Ulgenfeurg <duhem@mail.ru>
2017-06-26 23:32:14 +0300

    Update hw02_easy.py

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -16,3 +16,22 @@
 # Задача-3:
 # Дан произвольный список из целых чисел. Получите НОВЫЙ список из элементов исходного выполнив следующие условия:
 # если элемент кратный двум, то разделить его на 4, если не кратен, то умножить на два.
+
+# Задача-1:
+
+
+# Задача-2:
+Ramm = {"Till", "Richard", "Oliver", "Flake", "Cristoph", "Paul"}
+Guitarists = {"Paul", "Richard", "Oliver", "James", "Kirk"}
+print(Ramm&Guitarists)
+
+# Задача-3:
+
+Numbers = {1,2,3,4,5,6,7,8,9}
+New_Numbers = []
+for number in Numbers:
+	if (number % 2 == 0):
+		New_Numbers[:0] = [number / 4]
+	else:
+		New_Numbers[:0] = [number * 2]
+print New_Numbers
